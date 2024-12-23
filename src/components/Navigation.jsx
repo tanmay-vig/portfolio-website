fd9c48bbd76add141af3285a3ea6ba14fbdfa941
Tanmay <cooltanmayvig@gmail.com>
2024-12-23 16:25:32 +0530

    favicon added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,41 +1,66 @@
-import { useState } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import '../styles/Navbar.css';
-
-
+import { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "../styles/Navbar.css";
+import logo from "../assets/logo.jpg";
 
 function Navigantion() {
-  const [expanded, setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
-  const handleNavClick = () => {
-    setExpanded(false);
-  };
-  return (
-    <Navbar
-      bg="dark"
-      variant="dark"
-      expand="lg"
-      sticky="top"
-      className="custom-navbar"
-      expanded={expanded}
-    >
-      <Container>
-        <Navbar.Brand href="#home" className="brand">Tanmay Vig</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto fw-bold">
-            <Nav.Link href="#hero" onClick={handleNavClick}>Home</Nav.Link>
-            <Nav.Link href="#achievements" onClick={handleNavClick}>Achievements</Nav.Link>
-            <Nav.Link href="#experience" onClick={handleNavClick}>Experience</Nav.Link>
-            <Nav.Link href="#tools" onClick={handleNavClick}>Tools</Nav.Link>
-            <Nav.Link href="#projects" onClick={handleNavClick}>Projects</Nav.Link>
-            <Nav.Link href="#positions" onClick={handleNavClick}>Positions</Nav.Link>
-            <Nav.Link href="#contact" onClick={handleNavClick}>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    const handleNavClick = () => {
+        setExpanded(false);
+    };
+    return (
+        <Navbar
+            bg="dark"
+            variant="dark"
+            expand="lg"
+            sticky="top"
+            className="custom-navbar"
+            expanded={expanded}
+        >
+            <Container>
+                <Navbar.Brand href="#home" className="brand">
+                    <img
+                        src={logo}
+                        alt="logo"
+                        style={{ height: "35px", width: "35px" }}
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="#home" className="brand">
+                    Tanmay Vig
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={() => setExpanded(expanded ? false : true)}
+                />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto fw-bold">
+                        <Nav.Link href="#hero" onClick={handleNavClick}>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link href="#achievements" onClick={handleNavClick}>
+                            Achievements
+                        </Nav.Link>
+                        <Nav.Link href="#experience" onClick={handleNavClick}>
+                            Experience
+                        </Nav.Link>
+                        <Nav.Link href="#tools" onClick={handleNavClick}>
+                            Tools
+                        </Nav.Link>
+                        <Nav.Link href="#projects" onClick={handleNavClick}>
+                            Projects
+                        </Nav.Link>
+                        <Nav.Link href="#positions" onClick={handleNavClick}>
+                            Positions
+                        </Nav.Link>
+                        <Nav.Link href="#contact" onClick={handleNavClick}>
+                            Contact
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Navigantion;
